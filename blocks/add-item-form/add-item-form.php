efa467203a8ab3ef46cbe0ad2bf0d5af0f32d48a
haszari <haszari@cartoonbeats.com>
2022-06-18 19:30:49 +1200

    Tidy plugin metadata, title and bump (next) version

--- blocks/add-item-form/add-item-form.php
+++ blocks/add-item-form/add-item-form.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Plugin Name:       Add Item Form
- * Description:       Example static block scaffolded with Create Block tool.
+ * Plugin Name:       CBR Simple List
+ * Description:       WordPress plugin for adding items (posts) quickly from front end of site.
  * Requires at least: 5.8
  * Requires PHP:      7.0
- * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Version:           0.2.0
+ * Author:            haszari
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       add-item-form
+ * Text Domain:       cbr-simple-list
  *
  * @package           create-block
  */
